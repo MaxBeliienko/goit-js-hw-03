73bcf627f7577d6939ad183aaf59b6db7d822747
MaxBeliienko <mbelienko@gmail.com>
2023-12-02 13:13:40 +0200

    hw-3-final

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,18 @@
-"use strict";
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    const endMailTask2 = `Shipping to ${country} will cost ${totalPrice} credits`;
-    return endMailTask2;
+function makeArray(firstArray, secondArray, maxLength) {
+    const concatenatedArray = firstArray.concat(secondArray);
+    let finalArray = [];
+    if (concatenatedArray.length >= maxLength) {
+        finalArray = concatenatedArray.slice(0, maxLength)
+    } else {
+        finalArray = concatenatedArray;
+    }
+    return finalArray;
 }
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
